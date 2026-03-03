6261673d723a94349feada8e3f9169a0e5d4e63f
Ruth Reyes <ruth@kali>
2026-03-02 20:39:16 -0600

    Syscall 100 para encender el engine ya agregada

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,15 @@
 #include "easm.h"
+#include "MipsDisplay.hpp"
 
 extern "C" ErrorCode handleSyscall(uint32_t *regs, void *mem, MemoryMap *mem_map)
 {
     unsigned v0 = regs[Register::v0];
-
+    static MipsDisplay display;
     switch (v0)
     {
+        case 100:
+            display.RunEngine();
+            return ErrorCode::Ok;
         default:
             return ErrorCode::SyscallNotImplemented;
     }
