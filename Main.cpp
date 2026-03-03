44b6a60ecf7b8a3d6517a50d383a5cbddc732a9b
Ruth Reyes <ruth@kali>
2026-03-02 20:56:41 -0600

    Syscall 103 clear agregada

--- Main.cpp
+++ Main.cpp
@@ -10,6 +10,9 @@ extern "C" ErrorCode handleSyscall(uint32_t *regs, void *mem, MemoryMap *mem_map
         case 100:
             display.RunEngine();
             return ErrorCode::Ok;
+        case 103:
+            display.Clear(regs[Register::a0]);
+            return ErrorCode::Ok;
         default:
             return ErrorCode::SyscallNotImplemented;
     }
